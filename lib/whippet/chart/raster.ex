c8bf9250c177c925b4612cc52f2124727705ecaf
Jonathan Huang <grokut@gmail.com>
2017-05-22 09:36:11 -0700

    raster selection tool helpers

--- lib/whippet/chart/raster.ex
+++ lib/whippet/chart/raster.ex
@@ -1,9 +1,9 @@
 defmodule Whippet.Chart.Raster do
   use Hound.Helpers
+  import Beagle.Helpers
 
   def is_valid(selector, spec) do
     displayed = canvas_displayed?(selector, spec.use_map)
-
     if spec.legend do
       legend_valid = Whippet.Legend.Ordinal.is_valid(selector, spec.legend)
       displayed && legend_valid
@@ -18,5 +18,61 @@ defmodule Whippet.Chart.Raster do
     else
       element_displayed?({:css, "#{selector} canvas.webgl-canvas"})
     end
+  end
+
+  def draw_circle(selector, x, y) do
+    click find_element(:css, ".mapd-draw-button-circle")
+    move_click(selector, x, y)
+    :timer.sleep(Application.get_env(:whippet, :animation_timeout) * 2)
+  end
+
+  def draw_polyline(selector, points) do
+    click find_element(:css, ".mapd-draw-button-polyline")
+
+    points
+      |> Enum.each(fn({x, y}) -> move_click(selector, x, y) end)
+
+    points
+      |> Enum.take(-1)
+      |> Enum.at(0)
+      |> (fn({x, y}) -> move_doubleclick(selector, x, y) end).()
+
+    IO.inspect Application.get_env(:whippet, :animation_timeout)
+    :timer.sleep(Application.get_env(:whippet, :animation_timeout) * 2)
+  end
+
+  def draw_lasso(selector, points) do
+    click find_element(:css, ".mapd-draw-button-lasso")
+
+    points
+      |> Stream.with_index
+      |> Enum.to_list
+      |> Enum.each(fn({{x, y}, index}) ->
+        move_to({:css, "#{selector} canvas"}, x, y)
+        cond do
+          index == 0 -> mouse_down()
+          index == length(points) - 1 -> mouse_up()
+          true -> nil
+        end
+      end)
+
+    :timer.sleep(Application.get_env(:whippet, :animation_timeout) * 2)
+  end
+
+  def remove_selection(selector, x, y) do
+    move_click(selector, x, y)
+    send_keys(:backspace)
+    :timer.sleep(Application.get_env(:whippet, :animation_timeout) * 2)
+  end
+
+  defp move_click(selector, x, y) do
+    move_to({:css, "#{selector} canvas"}, x, y)
+    mouse_down()
+    mouse_up()
+  end
+
+  defp move_doubleclick(selector, x, y) do
+    move_to({:css, "#{selector} canvas"}, x, y)
+    doubleclick()
   end
 end
